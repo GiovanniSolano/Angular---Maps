f554a0af5d850f38fded36e63fea11103e6cbdd3
Giovanni Solano <giovannisolano211@gmail.com>
2021-02-10 19:53:58 -0600

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MapaLibreriaComponent } from './components/mapa-libreria/mapa-libreria.component';
+import { MapaNativoComponent } from './components/mapa-nativo/mapa-nativo.component';
+
+const routes: Routes = [
+
+  { path: 'nativo', component: MapaNativoComponent},
+  { path: 'libreria', component: MapaLibreriaComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/mapa-nativo/mapa-nativo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-nativo',
+  templateUrl: './mapa-nativo.component.html',
+  styleUrls: ['./mapa-nativo.component.css']
+})
+export class MapaNativoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
